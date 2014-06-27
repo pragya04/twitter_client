a795b16472005a45c3447ee17b89f482e0fa04ae
pherwani <pherwani@yahoo-inc.com>
2014-06-26 23:59:26 -0700

    Initial Commit

---
+++ TwitterClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TwitterClient
+//
+//  Created by Pragya  Pherwani on 6/26/14.
+//  Copyright (c) 2014 Pragya  Pherwani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "twitterAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([twitterAppDelegate class]));
+    }
+}
---
+++ TwitterClient/twitterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  twitterAppDelegate.h
+//  TwitterClient
+//
+//  Created by Pragya  Pherwani on 6/26/14.
+//  Copyright (c) 2014 Pragya  Pherwani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface twitterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
